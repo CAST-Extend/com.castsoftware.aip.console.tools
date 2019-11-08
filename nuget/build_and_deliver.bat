0e3e0d1e81fcc387a5c6100bfc467bae76d413bc
ABR <a.brizard@castsoftware.com>
2019-11-08 19:21:39 +0100

    Release version 1.1.0

--- nuget/build_and_deliver.bat
+++ nuget/build_and_deliver.bat
@@ -12,8 +12,8 @@ set CMDPATH=%0
 
 cd /d %WORKSPACE%
 
-set VERSION=1.0.6
-set ID=com.castsoftware.uc.aip.console.tools
+set VERSION=1.1.0
+set ID=com.castsoftware.aip.console.tools
 
 set PATH=%PATH%;c:\Tools\Git\usr\bin;C:\CAST-Caches\Win64
 
