c841e0a3f7a54e9033fefb2cb3d128edce0a57f1
lfo <l.fontanger@castsoftware.com>
2020-10-08 11:56:16 +0200

    WEBITOOLS-20: updates due to code review

--- aip-console-tools-core/src/main/java/com/castsoftware/aip/console/tools/core/utils/LogUtils.java
+++ aip-console-tools-core/src/main/java/com/castsoftware/aip/console/tools/core/utils/LogUtils.java
@@ -1,32 +1,31 @@
 package com.castsoftware.aip.console.tools.core.utils;
 
-import com.google.common.collect.Lists;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class LogUtils {
-    private static final String APIKEY_REGEXP = "\\-{2}apiKey[:|\\=]?\\s*(?<sensitiveInfo>\\S+)";
-    private static final String HOST_REGEXP = "\\*\\*\\s*HOST:\\s*(?<sensitiveInfo>\\S+)";
-    private static final String PORT_REGEXP = "\\*\\*\\s*PORT:\\s*(?<sensitiveInfo>\\d+)";
     private static final String HOST_PORT_REGEXP = "\\s*[_']?\\s*(?<sensitiveInfo>\\S+:\\d+)";
-    private static final String CONNECTION_REGEXP_1 = "(CastStorageService" + HOST_PORT_REGEXP + ")";
-    private static final String CONNECTION_REGEXP_2 = "____Connected to" + HOST_PORT_REGEXP + " database";
-    private static final String CONNECTION_REGEXP_3 = "Server" + HOST_PORT_REGEXP + " on CastStorageService'";
-    private static final String LOGFILE_REGEXP = "(-logFilePath|Log file path):\\s*(?<sensitiveInfo>\\S+)";
-    private static final String LICENSE_REGEXP = "-licenseKey\\s*:\\s*(?<sensitiveInfo>.+)";
     public static final String REPLACEMENT_STR = "****";
 
-    private static List<Pattern> SENSITIVE_INFO_PATTERNS = Lists.newArrayList(
-            Pattern.compile(HOST_REGEXP), Pattern.compile(PORT_REGEXP),Pattern.compile(APIKEY_REGEXP),
-            Pattern.compile(CONNECTION_REGEXP_1), Pattern.compile(CONNECTION_REGEXP_2), Pattern.compile(CONNECTION_REGEXP_3),
-            Pattern.compile(LOGFILE_REGEXP), Pattern.compile(LICENSE_REGEXP));
+    private static List<Pattern> SENSITIVE_INFO_PATTERNS = new ArrayList<>();
+    static{
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("\\*\\*\\s*HOST:\\s*(?<sensitiveInfo>\\S+)"));
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("\\*\\*\\s*PORT:\\s*(?<sensitiveInfo>\\d+)"));
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("\\-{2}apiKey[:|\\=]?\\s*(?<sensitiveInfo>\\S+)"));
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("(CastStorageService" + HOST_PORT_REGEXP + ")"));
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("____Connected to" + HOST_PORT_REGEXP + " database"));
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("Server" + HOST_PORT_REGEXP + " on CastStorageService'"));
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("(-logFilePath|Log file path):\\s*(?<sensitiveInfo>\\S+)"));
+        SENSITIVE_INFO_PATTERNS.add(Pattern.compile("-licenseKey\\s*:\\s*(?<sensitiveInfo>.+)"));
+    }
 
     public static String replaceAllSensitiveInformation(String toMath) {
-        Matcher m = SENSITIVE_INFO_PATTERNS.stream().parallel().map(pat -> pat.matcher(toMath)).filter(match -> match.find()).findFirst().orElse(null);
-        if (m != null) {
-            String what = m.group("sensitiveInfo");
+        Optional<Matcher> m = SENSITIVE_INFO_PATTERNS.stream().parallel().map(pat -> pat.matcher(toMath)).filter(match -> match.find()).findFirst();
+        if (m.isPresent()) {
+            String what = m.get().group("sensitiveInfo");
             return toMath.replace(what, REPLACEMENT_STR);
         }
         return toMath;
