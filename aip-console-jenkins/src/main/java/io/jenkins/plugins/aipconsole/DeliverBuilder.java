99109239c01b2616f29f8a8272f8f6121b3be08b
leon2Meudon92 <l.fontanger@castsoftware.com>
2021-05-31 10:56:48 +0200

    WEBITOOLS-33 stack trace hidden with debug options

--- aip-console-jenkins/src/main/java/io/jenkins/plugins/aipconsole/DeliverBuilder.java
+++ aip-console-jenkins/src/main/java/io/jenkins/plugins/aipconsole/DeliverBuilder.java
@@ -589,7 +589,7 @@ public class DeliverBuilder extends Builder implements SimpleBuildStep {
             taskListener.error("Failed to download the delivery report", e.getMessage());
         }
     }
-    
+
     /**
      * Check some initial elements before running the Job
      *
