d8184fcdcf42186dddad6421fa57b3c1d7647ecc
leon2Meudon92 <l.fontanger@castsoftware.com>
2022-08-30 16:51:53 +0200

    WEBITOOLS-129: Added create application integration test batch

---
+++ aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/create-application-integ-test.bat
@@ -0,0 +1,32 @@
+@echo off
+REM -- Create Application --
+SET TOOLS_CLI_PATH=%~1
+SET SERVER_URL=%~2
+SET API_KEY=%~3
+SET APP_NAME=%~4
+SET IN_PLACE_MODE=%~5
+SET DOMAIN_NAME=%~6
+SET CSS=%~7
+
+REM EXAMPLE
+REM create-application-integ-test.bat "download folder" "http://machine.corp.castsoftware.com:8081" "LPZ5i8lJ.5dKr2Y4e39cVIJ70rJETgG0sY29C2ElH" "WEBITOOLS-102-Test-App" false "TOOLS-CLI-TEST_DOM"
+REM
+SET MORE_OPTIONS=
+if not "%DOMAIN_NAME%" == "" SET MORE_OPTIONS=--domain-name="%DOMAIN_NAME%"
+if not "%IN_PLACE_MODE%" == "" SET MORE_OPTIONS=%MORE_OPTIONS% --no-version-history=%IN_PLACE_MODE%
+if not "%CSS%" == "" SET MORE_OPTIONS=%MORE_OPTIONS% --css-server=%CSS%
+
+echo -- Create Application command --
+echo java -jar aip-console-tools-cli.jar CreateApplication --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
+--app-name="%APP_NAME%" --verbose=false ^
+%MORE_OPTIONS% 
+echo --------------------------------
+
+CD /d "%TOOLS_CLI_PATH%"
+
+java -jar aip-console-tools-cli.jar CreateApplication --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
+	--app-name="%APP_NAME%" --verbose=false ^
+	%MORE_OPTIONS% 
+	
+echo exit code=%errorlevel%	
+
