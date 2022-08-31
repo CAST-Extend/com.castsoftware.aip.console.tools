0e16ebe2ccb91f55f0ce0a7fd51f4799717198d3
leon2Meudon92 <l.fontanger@castsoftware.com>
2022-08-31 13:42:59 +0200

    WEBITOOLS-129: download target Console tools version from extend

--- aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/create-application-integ-test.bat
+++ aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/create-application-integ-test.bat
@@ -7,6 +7,8 @@ SET APP_NAME=%~4
 SET IN_PLACE_MODE=%~5
 SET DOMAIN_NAME=%~6
 SET CSS=%~7
+set TOOLS_EXTENSION=com.castsoftware.aip.console.tools
+set EXTEND_URL=https://extend.castsoftware.com
 
 REM EXAMPLE
 REM create-application-integ-test.bat "download folder" "http://machine.corp.castsoftware.com:8081" "LPZ5i8lJ.5dKr2Y4e39cVIJ70rJETgG0sY29C2ElH" "WEBITOOLS-102-Test-App" false "TOOLS-CLI-TEST_DOM"
@@ -15,6 +17,30 @@ SET MORE_OPTIONS=
 if not "%DOMAIN_NAME%" == "" SET MORE_OPTIONS=--domain-name="%DOMAIN_NAME%"
 if not "%IN_PLACE_MODE%" == "" SET MORE_OPTIONS=%MORE_OPTIONS% --no-version-history=%IN_PLACE_MODE%
 if not "%CSS%" == "" SET MORE_OPTIONS=%MORE_OPTIONS% --css-server=%CSS%
+
+for %%a in (TOOLS_VERSION EXTEND_API_KEY) do (
+	if not defined %%a (
+			@echo.
+			@echo ERROR : Environment variable %%a should exist as environment var...
+			@echo.
+			exit /b 1
+	)
+)
+
+if not defined TOOLSDIR (
+	set TOOLSDIR=%WORKSPACE%\bin
+)
+echo -- Creating %TOOLSDIR% folder --
+if exist %TOOLSDIR% rmdir /S/Q %TOOLSDIR%
+mkdir %TOOLSDIR%
+
+echo ================================
+echo -------- Downloading extension from CAST Extend... -----------
+echo %TOOLS_EXTENSION% with version %TOOLS_VERSION%
+echo.
+
+curl -X GET "%EXTEND_URL%/api/package/download/%TOOLS_EXTENSION%/%TOOLS_VERSION%" -H "x-nuget-apikey: %EXTEND_API_KEY%" --output %TOOLSDIR%\%TOOLS_EXTENSION%.zip
+if errorlevel 1 goto endclean
 
 echo -- Create Application command --
 echo java -jar aip-console-tools-cli.jar CreateApplication --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
