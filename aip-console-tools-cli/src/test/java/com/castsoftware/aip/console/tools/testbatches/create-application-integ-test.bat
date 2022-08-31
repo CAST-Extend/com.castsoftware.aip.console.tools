3a2b57d326798732f79897def06460dc844e38c0
leon2Meudon92 <l.fontanger@castsoftware.com>
2022-08-31 15:05:00 +0200

    WEBITOOLS-129: download target Console tools version from extend

--- aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/create-application-integ-test.bat
+++ aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/create-application-integ-test.bat
@@ -47,7 +47,7 @@ echo ----------------------------
 echo Unzip the downloaded extension
 echo 7z.exe x -y -o. "%TOOLSDIR%\%TOOLS_EXTENSION%.zip"
 echo ----------------------------
-7z.exe x "%TOOLSDIR%\%TOOLS_EXTENSION%.zip" -y -o. "%TOOLSDIR%\%TOOLS_EXTENSION%"
+7z.exe x -y -o. "%TOOLSDIR%\%TOOLS_EXTENSION%.zip"
 
 echo -- Create Application command --
 echo java -jar aip-console-tools-cli.jar CreateApplication --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
