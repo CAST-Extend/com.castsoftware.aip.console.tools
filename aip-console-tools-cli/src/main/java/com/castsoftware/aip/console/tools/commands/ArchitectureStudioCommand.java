a899a7760d1259833090644943c9b9274a7d02a9
ANJ <a.ranjan2@castsoftware.com>
2023-04-28 14:06:46 +0530

    WEBITOOLS-175: Modifies endpoint to check model against an application

--- aip-console-tools-cli/src/main/java/com/castsoftware/aip/console/tools/commands/ArchitectureStudioCommand.java
+++ aip-console-tools-cli/src/main/java/com/castsoftware/aip/console/tools/commands/ArchitectureStudioCommand.java
@@ -60,37 +60,62 @@ public class ArchitectureStudioCommand extends BasicCollable{
     @Override
     public Integer processCallCommand() throws Exception {
         if (StringUtils.isBlank(modelName)) {
-            log.error("Model name should not be empty.");
+            log.error("Architecture model name should not be empty.");
             return Constants.RETURN_APPLICATION_INFO_MISSING;
         }
-        log.info(String.format("Getting model: %s", modelName));
+        log.info("Getting all architecture models");
         Set<ArchitectureModelDto> modelDtoSet = archService.getArchitectureModels();
+        log.info("Available Architecture Models:");
+        log.info(String.valueOf(modelDtoSet));
+
 
         if(modelDtoSet.isEmpty()){
+            log.info("No archutecture models available");
+            log.info(String.format("%s not found in available architecture models list", modelName));
             return Constants.RETURN_ARCHITECTURE_MODEL_NOT_FOUND;
         }
+
         /* Search name of the model in the list of available models and get the model details. */
-        ArchitectureModelDto modelInUse = modelDtoSet.stream().filter(m -> m.getName().equalsIgnoreCase(modelName)).collect(Collectors.collectingAndThen(Collectors.toSet(),
-                (Set<ArchitectureModelDto> dtoSet) -> dtoSet.isEmpty() ? null : dtoSet.iterator().next()));
+        ArchitectureModelDto modelInUse = modelDtoSet
+                .stream()
+                .peek(m -> log.info("Name property value: " + m.getName()))
+                .filter(m -> m.getName().equalsIgnoreCase(modelName))
+                .findFirst()
+                .orElse(null);
+
 
         //Check if model list is empty
         if (modelInUse == null){
-            log.error(String.format("Architecture model '{}' could not be found.", modelName));
+            log.error(String.format("Architecture model %s could not be found.", modelName));
             return Constants.RETURN_ARCHITECTURE_MODEL_NOT_FOUND;
         }
 
+        String path = modelInUse.getPath();
+
         ApplicationDto app = applicationService.getApplicationFromName(applicationName);
         if (app == null){
-            log.error(String.format("Application '{}' could not be found.", applicationName));
+            log.error(String.format("Application %s could not be found.", applicationName));
             return Constants.RETURN_APPLICATION_NOT_FOUND;
         }
+        log.info(String.format("Checking architecture model with app: %s", applicationName));
 
-        String path = modelInUse.getPath();
         log.info("App '{}' successful", applicationName);
 
-        Set<ArchitectureModelLinkDto> checkModel = archService.modelChecker(app.getGuid(), modelInUse.getPath(), app.getCaipVersion());
-        log.info("Violations for architecture model: %s", modelName);
-        log.info(String.valueOf(checkModel));
+        Set<ArchitectureModelLinkDto> checkModel = archService.modelChecker(app.getGuid(), path, app.getCaipVersion());
+        log.info("Violations for architecture model: '{}'", modelName);
+
+        //Print the result
+        log.info(String.format("| %10s | %15s | %10s | %10s | %15s | %10s |",
+                "Caller Name", "Caller Type", "Callee Name",
+                "Callee Type", "Link Type", "Dynamic Info"));
+        log.info("+------------+-----------------+------------+-----------------+---------------+--------------------------------+");
+        for (ArchitectureModelLinkDto dto : checkModel) {
+            log.info(String.format("| %10s | %15s | %10s | %10s | %15s | %10s |",
+                    dto.getCallerName(), dto.getCallerType(), dto.getCalleeName(),
+                    dto.getCalleeType(), dto.getLinkType(), dto.getDynamicInfo()));
+        }
+        log.info("+------------+-----------------+------------+-----------------+---------------+--------------------------------+");
+
         return Constants.RETURN_OK;
     }
 
