d26c8167f8d4c23ccf2fef1b60cee7f482dcb9a7
ANJ <a.ranjan2@castsoftware.com>
2023-05-04 13:35:40 +0530

    WEBITOOLS-175: Null check for response body

--- aip-console-tools-core/src/main/java/com/castsoftware/aip/console/tools/core/services/ArchitectureStudioServiceImpl.java
+++ aip-console-tools-core/src/main/java/com/castsoftware/aip/console/tools/core/services/ArchitectureStudioServiceImpl.java
@@ -2,13 +2,18 @@ package com.castsoftware.aip.console.tools.core.services;
 
 import com.castsoftware.aip.console.tools.core.dto.architecturestudio.ArchitectureModelDto;
 import com.castsoftware.aip.console.tools.core.dto.architecturestudio.ArchitectureModelLinkDto;
+import com.castsoftware.aip.console.tools.core.dto.jobs.CheckModelReportRequest;
 import com.castsoftware.aip.console.tools.core.dto.jobs.PathRequest;
 import com.castsoftware.aip.console.tools.core.exceptions.ApiCallException;
 import com.castsoftware.aip.console.tools.core.exceptions.ApplicationServiceException;
 import com.castsoftware.aip.console.tools.core.utils.ApiEndpointHelper;
+import com.castsoftware.aip.console.tools.core.utils.FileUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.java.Log;
-
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 @Log
@@ -32,11 +37,49 @@ public class ArchitectureStudioServiceImpl implements ArchitectureStudioService 
 
     public Set<ArchitectureModelLinkDto> modelChecker(String appGuid, String path, String caipVersion) throws ApiCallException {
 
-        PathRequest pathRequest = PathRequest.builder().path(path).build();
+        PathRequest pathRequest = PathRequest
+                .builder()
+                .path(path)
+                .build();
         return restApiService.postForEntity(
                 ApiEndpointHelper.getModelCheckUrl(appGuid),
                 pathRequest,
                 new TypeReference<Set<ArchitectureModelLinkDto>>(){}
         );
     }
+
+    public void downloadCheckedModelReport(String appGuid, String modelName, Integer metricId, String description, Integer transactionId, Set<ArchitectureModelLinkDto> checkModel) throws Exception {
+
+        CheckModelReportRequest checkModelReportRequest = CheckModelReportRequest
+                .builder()
+                .description(description)
+                .links(checkModel)
+                .metricId(metricId)
+                .name(modelName)
+                .transactionId(transactionId)
+                .build();
+
+        String modelUrl = ApiEndpointHelper.getDownlaodModelCheckUrl(appGuid);
+
+        Response response = restApiService.exchangeForResponse(
+                "POST",
+                modelUrl,
+                checkModelReportRequest
+        );
+        ResponseBody responseBody = response.body();
+
+
+        String filename = getDownloadedFileName(modelName);
+        //downloads the report
+        if(responseBody != null) {
+            FileUtils.fileDownload(filename, responseBody);
+        }
+    }
+
+    private String getDownloadedFileName(String modelName) {
+        String cleanedModelName = modelName.replaceAll("[\\\\/:*?\"<>|]+", "");
+
+        return String.format("%s_%s.xlsx", cleanedModelName, DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss").format(LocalDateTime.now()));
+    }
+
 }
