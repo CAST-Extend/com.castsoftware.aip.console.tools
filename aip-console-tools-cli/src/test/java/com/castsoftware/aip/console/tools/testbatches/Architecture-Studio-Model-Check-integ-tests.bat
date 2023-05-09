8f1d50e4b68444030751367f1c83c59ea2f16001
ANJ <a.ranjan2@castsoftware.com>
2023-05-09 15:42:52 +0530

    WEBITOOLS-175: Added bat file for architecture studio model check

---
+++ aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/Architecture-Studio-Model-Check-integ-tests.bat
@@ -0,0 +1,38 @@
+@echo off
+REM -- Check architecture model against an application --
+SET SERVER_URL=%~1
+SET API_KEY=%~2
+SET APP_NAME=%~3
+SET MODEL_CHECK_REPORT_PATH=%~4
+SET MODEL_NAME=%~5
+set TOOLS_EXTENSION=com.castsoftware.aip.console.tools
+set EXTEND_URL=https://extend.castsoftware.com
+set PATH=C:\CAST-Caches\Win64;%PATH%
+
+for %%a in ( TOOLSDIR ) do (
+	if not defined %%a (
+			@echo.
+			@echo ERROR : Environment variable %%a should exist as environment var...
+			@echo.
+			exit /b 1
+	)
+)
+echo ----------------------------
+echo Unziping the downloaded build artifact
+echo 7z.exe x "%TOOLSDIR%\%TOOLS_EXTENSION%.zip" -y -o"%TOOLSDIR%\%TOOLS_EXTENSION%"
+echo ----------------------------
+7z.exe x "%TOOLSDIR%\%TOOLS_EXTENSION%.zip" -y -o"%TOOLSDIR%\%TOOLS_EXTENSION%"
+
+echo -- Check architecture model against an application --
+echo java -jar aip-console-tools-cli.jar ArchitectureStudioModelCheck --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
+     	--app-name="%APP_NAME%"  --model-name="%MODEL_NAME%" --verbose=%VERBOSE%
+echo --------------------------------
+
+SET TOOLS_CLI_PATH=%TOOLSDIR%\%TOOLS_EXTENSION%
+CD /d "%TOOLS_CLI_PATH%"
+
+java -jar aip-console-tools-cli.jar ArchitectureStudioModelCheck --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
+	--app-name="%APP_NAME%"  --model-name="%MODEL_NAME%" --verbose=%VERBOSE%
+
+echo exit code=%errorlevel%
+
