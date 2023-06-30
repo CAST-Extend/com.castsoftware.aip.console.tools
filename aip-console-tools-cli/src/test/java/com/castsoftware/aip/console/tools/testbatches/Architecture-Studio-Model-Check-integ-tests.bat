d1b8d2b2881e9036a4a8fb77103440b6268dc40e
ANJ <a.ranjan2@castsoftware.com>
2023-06-30 15:09:13 +0530

    WEBITOOLS-185: Name change for integartion test

--- aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/Architecture-Studio-Model-Check-integ-tests.bat
+++ aip-console-tools-cli/src/test/java/com/castsoftware/aip/console/tools/testbatches/Architecture-Studio-Model-Check-integ-tests.bat
@@ -25,14 +25,14 @@ echo ----------------------------
 7z.exe x "%TOOLSDIR%\%TOOLS_EXTENSION%.zip" -y -o"%TOOLSDIR%\%TOOLS_EXTENSION%"
 
 echo -- Check architecture model against an application --
-echo java -jar aip-console-tools-cli.jar ArchitectureStudioModelCheck --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
+echo java -jar aip-console-tools-cli.jar ArchitectureStudioModelChecker --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
      	--app-name="%APP_NAME%"  --model-name="%MODEL_NAME%" --report-path="%MODEL_CHECK_REPORT_PATH" --file-path="%UPLOAD_FILE_PATH" --verbose=%VERBOSE%
 echo --------------------------------
 
 SET TOOLS_CLI_PATH=%TOOLSDIR%\%TOOLS_EXTENSION%
 CD /d "%TOOLS_CLI_PATH%"
 
-java -jar aip-console-tools-cli.jar ArchitectureStudioModelCheck --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
+java -jar aip-console-tools-cli.jar ArchitectureStudioModelChecker --server-url="%SERVER_URL%" --apikey="%API_KEY%" --timeout=5000 ^
 	--app-name="%APP_NAME%"  --model-name="%MODEL_NAME%" --report-path="%MODEL_CHECK_REPORT_PATH" --file-path="%UPLOAD_FILE_PATH" --verbose=%VERBOSE%
 
 echo exit code=%errorlevel%
