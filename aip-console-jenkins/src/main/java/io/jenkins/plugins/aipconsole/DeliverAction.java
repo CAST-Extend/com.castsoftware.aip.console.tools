7182e24526da100d42a3eca8e79e73c653d07ed3
ANJ <a.ranjan2@castsoftware.com>
2023-12-12 13:55:29 +0530

    WEBITOOLS-210: Adds security dataflow to deliver version command on jenkins

--- aip-console-jenkins/src/main/java/io/jenkins/plugins/aipconsole/DeliverAction.java
+++ aip-console-jenkins/src/main/java/io/jenkins/plugins/aipconsole/DeliverAction.java
@@ -23,7 +23,7 @@ public class DeliverAction extends BaseAction implements RunAction2 {
     private String cssServerName;
     private boolean cloneVersion = false;
     private boolean blueprint = false;
-    private boolean enableSecurityAssessment = false;
+    private boolean enableSecurityDataflow = false;
     private boolean enableDataSafety;
 
     @Nullable
@@ -109,12 +109,12 @@ public class DeliverAction extends BaseAction implements RunAction2 {
         this.blueprint = blueprint;
     }
 
-    public boolean isSecurityAssessmentEnabled() {
-        return enableSecurityAssessment;
+    public boolean isSecurityDataflowEnabled() {
+        return enableSecurityDataflow;
     }
 
-    public void setSecurityAssessment(boolean enableFlag) {
-        enableSecurityAssessment = enableFlag;
+    public void setEnableSecurityDataflow(boolean enableFlag) {
+        enableSecurityDataflow = enableFlag;
     }
 
     public void setEnableDataSafety(boolean flag) {
--- aip-console-jenkins/src/main/java/io/jenkins/plugins/aipconsole/DeliverBuilder.java
+++ aip-console-jenkins/src/main/java/io/jenkins/plugins/aipconsole/DeliverBuilder.java
@@ -101,7 +101,6 @@ public class DeliverBuilder extends BaseActionBuilder implements SimpleBuildStep
 
     private boolean cloneVersion = false;
     private boolean blueprint = false;
-    private boolean enableSecurityAssessment = false;
 
     @Nullable
     private String versionName = "";
@@ -179,19 +178,6 @@ public class DeliverBuilder extends BaseActionBuilder implements SimpleBuildStep
 
     public boolean isBlueprint() {
         return blueprint;
-    }
-
-    @DataBoundSetter
-    public void setEnableSecurityAssessment(boolean enableFlag) {
-        enableSecurityAssessment = enableFlag;
-    }
-
-    public boolean getEnableSecurityAssessment() {
-        return isSecurityAssessmentEnabled();
-    }
-
-    public boolean isSecurityAssessmentEnabled() {
-        return enableSecurityAssessment;
     }
 
     @DataBoundSetter
@@ -273,13 +259,17 @@ public class DeliverBuilder extends BaseActionBuilder implements SimpleBuildStep
         this.nodeName = nodeName;
     }
 
-    public boolean isEnableSecurityDataflow() {
+    public boolean isSecurityDataflowEnabled() {
         return enableSecurityDataflow;
     }
 
     @DataBoundSetter
     public void setEnableSecurityDataflow(boolean enableSecurityDataflow) {
         this.enableSecurityDataflow = enableSecurityDataflow;
+    }
+
+    public boolean getEnableSecurityDataflow() {
+        return isSecurityDataflowEnabled();
     }
 
     @DataBoundSetter
@@ -527,7 +517,7 @@ public class DeliverBuilder extends BaseActionBuilder implements SimpleBuildStep
                 requestBuilder.endStep(Constants.SET_CURRENT_STEP_NAME);
             }
             requestBuilder.objectives(VersionObjective.BLUEPRINT, isBlueprint());
-            requestBuilder.objectives(VersionObjective.SECURITY, isSecurityAssessmentEnabled());
+            requestBuilder.objectives(VersionObjective.SECURITY, isSecurityDataflowEnabled());
 
             String expandedExclusionPatterns = vars.expand(exclusionPatterns);
             log.println("Exclusion patterns : " + expandedExclusionPatterns);
