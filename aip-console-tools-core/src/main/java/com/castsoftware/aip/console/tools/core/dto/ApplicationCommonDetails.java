374818252cda11bfd5aa8b6ccd96327afe1f9638
ANJ <a.ranjan2@castsoftware.com>
2024-03-19 11:54:20 +0530

    WEBITOOLS-231: Review comment changes

--- aip-console-tools-core/src/main/java/com/castsoftware/aip/console/tools/core/dto/ApplicationCommonDetails.java
+++ aip-console-tools-core/src/main/java/com/castsoftware/aip/console/tools/core/dto/ApplicationCommonDetails.java
@@ -1,5 +1,6 @@
 package com.castsoftware.aip.console.tools.core.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +9,9 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class ApplicationCommonDetails {
 
+    @Builder.Default
     public List<ApplicationCommonDetailsDto> applicationCommonDetailsDtoSet = new ArrayList<>();
 }
